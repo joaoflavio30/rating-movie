d26f35919877c1bf3d6c5f8c4ebd81626d072a22
freitas <joaoflaviofreitas@gmail.com>
2023-11-30 09:52:44 -0300

    Refactor/some changes in imports

--- src/test/java/com/joaoflaviofreitas/openlabsdemo/service/MovieServiceTest.java
+++ src/test/java/com/joaoflaviofreitas/openlabsdemo/service/MovieServiceTest.java
@@ -17,7 +17,8 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
